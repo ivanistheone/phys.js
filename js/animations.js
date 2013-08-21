6b65476eb72dbecc4974b73b94cc0da65beddcc4
Ivan Savov <ivan.savov@gmail.com>
2013-08-21 16:46:26 -0400

    Added a click-and-drag interface for setting the velocity of the rocket.

--- js/animations.js
+++ js/animations.js
@@ -1,5 +1,7 @@
 
 
+
+
 var TARGET_POS = 600;
 
 function Rect(options) { 
@@ -47,15 +49,12 @@ if (this.y > maxHeight) this.y = 0;
 
 
 Rect.prototype.render = function (context) {
-
-if (this.img){
-context.drawImage(this.img,this.x,this.y-50,120,100);
-} else {
-context.fillStyle = this.fill
-context.fillRect(this.x, this.y, this.width, this.height)
-}
-
-
+    if (this.img){
+        context.drawImage(this.img,this.x,this.y-50,120,100);
+    } else {
+        context.fillStyle = this.fill
+        context.fillRect(this.x, this.y, this.width, this.height)
+    }
 }
 
 
@@ -70,14 +69,17 @@ this.canvas = canvas;
 this.canvas.height = options.height || this.canvas.height;
 this.canvas.width  = options.width || this.canvas.width;
 this.context = canvas.getContext('2d')
+
 this.obj = []
 this._start = false
 this._clock = this._clock ? this._clock : new Date().getTime()
 
-this.canvas.addEventListener('click', (function (e) {
-  e.preventDefault()
-  this.start_animation();
-}).bind(this))
+    /*
+    this.canvas.addEventListener('click', (function (e) {
+      e.preventDefault()
+      this.start_animation();
+    }).bind(this))
+    */
 }
 
 
@@ -86,7 +88,7 @@ Anim.prototype.start_animation = function () {
     this._starttime = new Date().getTime()
 
 
-    this.obj[0].v_x= Number( this.dashboard.find(".velocity").text() );    
+    //this.obj[0].v_x= Number( this.dashboard.find(".velocity").text() );    
     
     this.start();
 
@@ -118,42 +120,43 @@ Anim.prototype.draw_xy_axes = function () {
 var ctx = this.context,
     arr_size = 8;
 
+    // x - axis
+    ctx.beginPath();
+    var xaxis_pos = this.canvas.height-10;
+    ctx.moveTo(0,xaxis_pos);
+    ctx.lineTo(this.canvas.width,xaxis_pos);
+    ctx.lineTo(this.canvas.width-arr_size,xaxis_pos+ 0.5*arr_size);
+    ctx.lineTo(this.canvas.width,xaxis_pos);    
+    ctx.lineTo(this.canvas.width-arr_size,xaxis_pos- 0.5*arr_size);
+    //ctx.lineTo(this.canvas.width,xaxis_pos);
+    //ctx.lineTo(this.canvas.width-arr_size,-arr_size);    
+    ctx.stroke();    
+    ctx.font = 'Italic 9px Sans-Serif';
+    ctx.strokeText("x", this.canvas.width - arr_size - 8, xaxis_pos+ 9);  
 
-// x - axis
-ctx.beginPath();
-var xaxis_pos = this.canvas.height-10;
-ctx.moveTo(0,xaxis_pos);
-ctx.lineTo(this.canvas.width,xaxis_pos);
-ctx.lineTo(this.canvas.width-arr_size,xaxis_pos+ 0.5*arr_size);
-ctx.lineTo(this.canvas.width,xaxis_pos);    
-ctx.lineTo(this.canvas.width-arr_size,xaxis_pos- 0.5*arr_size);
-//ctx.lineTo(this.canvas.width,xaxis_pos);
-//ctx.lineTo(this.canvas.width-arr_size,-arr_size);    
-ctx.stroke();    
-ctx.font = 'Italic 9px Sans-Serif';
-ctx.strokeText("x", this.canvas.width - arr_size - 8, xaxis_pos+ 9);  
-
-// y - axis
-ctx.beginPath();
-ctx.moveTo(0,0);
-ctx.lineTo(0,this.canvas.height);
-//    ctx.lineTo(arr_size, this.canvas.height-arr_size);
-//    ctx.lineTo(0, this.canvas.height);
-//    ctx.lineTo(-arr_size, this.canvas.height-arr_size);    
-ctx.stroke();    
-ctx.font = 'Italic 9px Sans-Serif';
-//    ctx.strokeText("y", 3, this.canvas.height - arr_size - 3);  
+    // y - axis
+    ctx.beginPath();
+    ctx.moveTo(0,0);
+    ctx.lineTo(0,this.canvas.height);
+    //    ctx.lineTo(arr_size, this.canvas.height-arr_size);
+    //    ctx.lineTo(0, this.canvas.height);
+    //    ctx.lineTo(-arr_size, this.canvas.height-arr_size);    
+    ctx.stroke();    
+    ctx.font = 'Italic 9px Sans-Serif';
+    //    ctx.strokeText("y", 3, this.canvas.height - arr_size - 3);  
 
 }
 
 Anim.prototype.render = function () {
-this.canvas.width = this.canvas.width  // clear
-this.draw_xy_axes()
-this.obj.forEach((function (o) {
-  o.render(this.context)
-}).bind(this))
+    this.canvas.width = this.canvas.width  // clear
+    this.draw_xy_axes()
+    this.obj.forEach((function (o) {
+      o.render(this.context)
+    }).bind(this))
 
-   
+    if (this._start){
+        this.dragUI.render();
+    }
 
 }
 
@@ -190,11 +193,11 @@ Anim.prototype.reset = function () {
 
     this.obj.push(r1);
     
-    // draw target
     r2 = new Rect({ x:TARGET_POS, y:50, width:5, height:100 })
     this.obj.push(r2);    
     
     this.render();
+
 
 }
 
@@ -209,6 +212,82 @@ Anim.prototype.isCollision = function () {
 
 }
 
+
+
+
+// click and drag UI
+var DragUI = function( anim ) {
+    this.anim = anim;
+    anim.dragUI = this;
+    this.mouse = {x: 0, y: 0, isDown: false};
+
+    // use jQuery $.proxy to bind `this` to the dragUI object inside event handlers 
+    $(anim.canvas).on("mousemove", $.proxy(this.render,this)        );
+    $(anim.canvas).on("mousedown", $.proxy(this.mouseDown,this)     );
+    $(anim.canvas).on("mouseup",   $.proxy(this.mouseUp,this)       );
+
+    console.log("initizlized DragUI");
+}
+
+
+
+// Draw the slingshot
+DragUI.prototype.render = function(e) {
+    //console.log("rendering...");
+    if (e) {
+         this.getMousePosition(e);      // if called with w=== mousemove 
+    }                                   // else could be called from Anim 
+
+    var ctx = this.anim.context;
+    if (this.mouse.isDown) {
+        if (!this.anim._start) {
+            this.anim.render(); 
+        }
+        ctx.beginPath();
+        ctx.moveTo(this.anim.obj[0].x, this.anim.obj[0].y);
+        ctx.lineTo(this.mouse.x, this.mouse.y);
+        ctx.stroke();
+        ctx.closePath();
+    }
+}
+
+DragUI.prototype.getMousePosition = function(e) {
+    var anim = this.anim,      // jQuery attached the anim object to the e.data ...
+        dragUI = this;
+    
+    // this function will be bound to the canvas 
+    dragUI.mouse.x = e.pageX - anim.canvas.offsetLeft;
+    dragUI.mouse.y = e.pageY - anim.canvas.offsetTop;
+}
+
+
+DragUI.prototype.mouseDown = function(e) {
+    var anim = this.anim,      // jQuery attached the anim object to the e.data ...
+        dragUI = this;
+    if (e.which == 1) {
+        dragUI.getMousePosition(e);
+        dragUI.mouse.isDown = true;
+        anim.obj[0].x = dragUI.mouse.x;
+        anim.obj[0].y = dragUI.mouse.y;
+    }
+}
+
+DragUI.prototype.mouseUp = function(e) { 
+    var anim = this.anim,      // jQuery attached the anim object to the e.data ...
+        dragUI = this,
+        ball = anim.obj[0];
+
+    this.getMousePosition(e);
+    if (e.which == 1) {
+        dragUI.mouse.isDown = false;
+        ball.v_y = (dragUI.mouse.y - ball.y) / 2;
+        ball.v_x = (dragUI.mouse.x - ball.x) / 2;
+    }
+
+    if (!anim._start) {
+      anim.start_animation();
+    }
+}
 
 
 
